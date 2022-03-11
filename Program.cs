0cf486bc5e5ab9eb7f693a5460473e7a55e0573f
Nita <nitaainun@student.telkomuniversity.ac.id>
2022-03-11 12:02:11 +0700

    menambahkan implementasi dengan teble-driven

--- Program.cs
+++ Program.cs
@@ -1,27 +1,25 @@
 ﻿using System;
 
 class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            KodePos table_Kodepos = new KodePos();
-            Console.WriteLine("=== GET KODE POS BATUNUNGGAL===");
-            table_Kodepos.getKodepos("Batununggal");
-            Console.WriteLine("==== GET ALL KODE POS ====");
-            table_Kodepos.getAllkodepos();
+        KodePos table_Kodepos = new KodePos();
+        Console.WriteLine("=== GET KODE POS BATUNUNGGAL===");
+        table_Kodepos.getKodepos("Batununggal");
+        Console.WriteLine("==== GET ALL KODE POS ====");
+        table_Kodepos.getAllkodepos();
 
-            Console.WriteLine("================== KUNCI PINTU ================");
-            DoorMachine pintu = new DoorMachine();
-            pintu.kunci();
+        
 
-
-        }
 
     }
-    class KodePos
-    {
 
-        Dictionary<string, string> dic = new Dictionary<string, string>() {
+}
+class KodePos
+{
+
+    Dictionary<string, string> dic = new Dictionary<string, string>() {
             {"Batununggal", "40266"},
             {"Kujangsari","40287" },
             {"Mengger"  ,"40267" },
@@ -34,59 +32,23 @@ class Program
             {"Maleer"   ,"40274"},
             {"Samoja"   ,"40273"} };
 
-        public void getKodepos(string kel)
-        {
-            if (dic.ContainsKey(kel))
-            {
-                Console.WriteLine(kel + " : " + dic[kel]);
-            }
-            else
-            {
-                Console.WriteLine(kel + " tidak ditemukan");
-            }
-        }
-
-        public void getAllkodepos()
-        {
-            foreach (KeyValuePair<string, string> ele1 in dic)
-            {
-                Console.WriteLine("{0} \t\t {1}", ele1.Key, ele1.Value);
-            }
-        }
-    }
-
-
-    class DoorMachine
+    public void getKodepos(string kel)
     {
-        enum State { TERKUNCI, TERBUKA };
-        public void kunci()
+        if (dic.ContainsKey(kel))
         {
-            State state = State.TERKUNCI;
-
-            String[] screenName = { "TERKUNCI", "TERBUKA" };
-            do
-            {
-                Console.WriteLine("PINTU " + screenName[(int)state]);
-                Console.Write("Enter Command : ");
-                String command = Console.ReadLine();
-                switch (state)
-                {
-                    case State.TERKUNCI:
-                        if (command == "BukaPintu")
-                        {
-                            state = State.TERBUKA;
-                        }
-                        break;
-                    case State.TERBUKA:
-                        if (command == "KunciPintu")
-                        {
-                            state = State.TERKUNCI;
-                        }
-                        break;
-
-
-
-                }
-            } while (state != State.TERKUNCI);
+            Console.WriteLine(kel + " : " + dic[kel]);
+        }
+        else
+        {
+            Console.WriteLine(kel + " tidak ditemukan");
         }
     }
+
+    public void getAllkodepos()
+    {
+        foreach (KeyValuePair<string, string> ele1 in dic)
+        {
+            Console.WriteLine("{0} \t\t {1}", ele1.Key, ele1.Value);
+        }
+    }
+}
